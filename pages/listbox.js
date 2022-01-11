044afbdda63df56b3dc4bc2fc48b868a8eeac1ee
decozzfx <m.f.r.ozil@gmail.com>
2022-01-11 07:31:18 +0700

    change listbox from static to dinamic

--- pages/listbox.js
+++ pages/listbox.js
@@ -1,24 +1,40 @@
 import { Listbox } from "@headlessui/react";
 import { useState } from "react";
 
-
+const countries = [
+    {
+        id : '1',
+        name : 'Indonesia'
+    },
+    {
+        id : '2',
+        name : 'Malaysia'
+    },
+    {
+        id : '3',
+        name : 'Philipina'
+    },
+    {
+        id : '4',
+        name : 'Laos'
+    },
+]
 
 export default function ListboxPage(){
-    const [selectedItem, setSelectedItem] = useState(1)
+    const [selectedItem, setSelectedItem] = useState('')
     return(
         <div className="flex items-center justify-center h-screen">
             <Listbox as='div' className='relative' onChange={(value) => setSelectedItem(value)}>
                 <Listbox.Button className='flex items-center border border-gray-500 rounded px-4 py-2 hover:bg-blue-400'>
-                Choose Country : {selectedItem}
+                {selectedItem === '' ? 'Choose Country' : `${selectedItem}`} 
                 <div className="flex">
                     <img src="/chevron-down.svg" className="ml-3 bg-indigo-600"/>
                 </div>
                 </Listbox.Button>
                 <Listbox.Options className='absolute w-56 rounded mt-2 border border-gray-300'>
-                    <Listbox.Option value='1' className='py-2 px-4 hover:bg-gray-100 cursor-pointer' >Indonesia</Listbox.Option>
-                    <Listbox.Option value='2' className='py-2 px-4 hover:bg-gray-100 cursor-pointer' >Malaysia</Listbox.Option>
-                    <Listbox.Option value='3' className='py-2 px-4 hover:bg-gray-100 cursor-pointer' >Philipina</Listbox.Option>
-                    <Listbox.Option value='4' className='py-2 px-4 hover:bg-gray-100 cursor-pointer' >Laos</Listbox.Option>
+                {countries.map(country => (
+                    <Listbox.Option key={country.id} value={country.name} className='py-2 px-4 hover:bg-gray-100 cursor-pointer' >{country.name}</Listbox.Option>
+                ))}
                 </Listbox.Options>
             </Listbox>
         </div>
